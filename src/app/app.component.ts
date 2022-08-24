9c864ef77ccea18162bf5a5bfcf345d1708af00d
dzebski <dzebovski@gmail.com>
2022-08-24 22:53:14 +0300

    SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core';
 import {ModalComponent} from "./modal/modal.component";
 import {RefDerective} from "./ref.derective";
+import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,19 @@ import {RefDerective} from "./ref.derective";
 export class AppComponent {
   // modal: boolean = false;
   @ViewChild(RefDerective, {static: false}) refDir!:RefDerective
-  constructor(private resolver: ComponentFactoryResolver) {
+
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private title: Title,
+    private meta: Meta
+    ) {
+    title.setTitle('AppComponentPage')
+    meta.addTags([
+      {name: 'keywords', content: 'google'},
+      {name: 'description', content: 'Hello World OZOZOZOOZ'}
+    ])
   }
+
   showModalWindow() {
   const modalFactory = this.resolver.resolveComponentFactory(ModalComponent)
     this.refDir.containerRef.clear()
